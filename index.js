c49255dc8577f3660c63880a4e28827caa0a86d8
tewyduisokri4 <osjcrlandobangz@hotmail.com>
2024-05-10 21:49:44 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const numbers = [1, 5, 2, 8];
-const max = Math.max(...numbers);
-console.log(max); // 8
+const pull = (arr, ...args) => {
+  let argState = Array.isArray(args[0]) ? args[0] : args;
+  let pulled = arr.filter((v, i) => !argState.includes(v));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+};
