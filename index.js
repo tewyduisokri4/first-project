f99908ced9f470541b66f978f5acc94a7eefb82d
tewyduisokri4 <osjcrlandobangz@hotmail.com>
2024-05-13 01:36:30 +0700

    test task

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function isPrime(n) {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  let i = 5;
-  while (i * i <= n) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-    i += 6;
-  }
-  return true;
+const number = 10;
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
 }
