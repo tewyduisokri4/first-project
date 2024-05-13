4591afe536297f117db87990e2386f8178530673
tewyduisokri4 <osjcrlandobangz@hotmail.com>
2024-05-13 18:25:10 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const number = 10;
-if (number % 2 === 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
+function isAlienSorted(words, order) {
+  const dict = new Map();
+  for (let i = 0; i < order.length; i++) {
+    dict.set(order[i], i);
+  }
+  for (let i = 0; i < words.length - 1; i++) {
+    const word1 = words[i];
+    const word2 = words[i + 1];
+    let found = false;
+    for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
+      if (word1[j] !== word2[j]) {
+        if (dict.get(word1[j]) > dict.get(word2[j])) return false;
+        found = true;
+        break;
+      }
+    }
+    if (!found && word1.length > word2.length) return false;
+  }
+  return true;
 }
