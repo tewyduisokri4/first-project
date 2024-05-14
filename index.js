a3116e00673c47f3905ba33a3eaf370c9357f988
tewyduisokri4 <osjcrlandobangz@hotmail.com>
2024-05-14 15:45:55 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-function isAlienSorted(words, order) {
-  const dict = new Map();
-  for (let i = 0; i < order.length; i++) {
-    dict.set(order[i], i);
-  }
-  for (let i = 0; i < words.length - 1; i++) {
-    const word1 = words[i];
-    const word2 = words[i + 1];
-    let found = false;
-    for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
-      if (word1[j] !== word2[j]) {
-        if (dict.get(word1[j]) > dict.get(word2[j])) return false;
-        found = true;
-        break;
-      }
+function subarraySum(nums, k) {
+  const map = new Map();
+  map.set(0, 1);
+  let count = 0;
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
     }
-    if (!found && word1.length > word2.length) return false;
+    map.set(sum, (map.get(sum) || 0) + 1);
   }
-  return true;
+  return count;
 }
