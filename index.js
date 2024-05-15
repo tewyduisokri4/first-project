de09678d7e2d18dd10a8837f77a57fee3b3145eb
tewyduisokri4 <osjcrlandobangz@hotmail.com>
2024-05-15 08:03:20 +0700

    feat

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function subarraySum(nums, k) {
-  const map = new Map();
-  map.set(0, 1);
-  let count = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
-  }
-  return count;
-}
+const firstName = "John";
+const lastName = "Doe";
+const fullName = firstName + " " + lastName;
+console.log(fullName); // John Doe
