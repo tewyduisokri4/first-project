033464a4658e232ff2fdb603d8faa7d3aeff7634
tewyduisokri4 <osjcrlandobangz@hotmail.com>
2024-05-16 20:18:31 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const firstName = "John";
-const lastName = "Doe";
-const fullName = firstName + " " + lastName;
-console.log(fullName); // John Doe
+function lengthOfLongestSubstring(s) {
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) {
+      left = Math.max(left, map.get(s[right]) + 1);
+    }
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
+}
